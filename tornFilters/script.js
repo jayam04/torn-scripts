37eb40c3444124cc66368cb7cc431ac1ada410f1
Jayam Patel <jayampatel04@gmail.com>
2024-11-27 09:14:54 +0530

    started Torn Filters

--- tornFilters/script.js
+++ tornFilters/script.js
@@ -1,30 +1,19 @@
 // ==UserScript==
-// @name         Torn Better UI
+// @name         Torn Filters
 // @namespace    http://tampermonkey.net/
 // @version      alpha-1.0
-// @description  Make Torn Beautiful
+// @description  Only Filter Script Everyone Needs
 // @author       Jayam Patel
 // @match        https://www.torn.com/*
-// @icon         https://raw.githubusercontent.com/jayam04/torn-scripts/master/tornBetterUI/icon.png
+// @icon         https://raw.githubusercontent.com/jayam04/torn-scripts/master/tornFilters/icon.png
 // @license      Apache License 2.0
 // @grant        none
 // @require      https://update.greasyfork.org/scripts/501645/1416051/Torn%20UI%20Elements%20%28for%20Developers%29.js
 // @require      https://update.greasyfork.org/scripts/501648/1416074/Constants%20for%20Torn%20Scripts%20%28for%20Developers%29.js
 // ==/UserScript==
 
-function realignTornUI() {
-  const mainContainer = document.getElementById("mainContainer");
-  mainContainer.style.marginLeft = "20px";
-}
-
-function updateFont() {
-  const mainContainer = document.getElementById("mainContainer");
-  mainContainer.style.fontFamily = "Monaco, monospace";
-}
-
 function main() {
-  realignTornUI();
-  updateFont();
+  console.log("Torn Filters Loaded");
 }
 
 main();
